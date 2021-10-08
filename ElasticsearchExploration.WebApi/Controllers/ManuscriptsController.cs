752cf5d65ca0eb1b472f38a02e5af6a54392cb44
Pablo Company <p.c.ramirez5150@gmail.com>
2021-10-08 17:17:06 +0200

    Initial commit

---
+++ ElasticsearchExploration.WebApi/Controllers/ManuscriptsController.cs
@@ -0,0 +1,44 @@
+﻿using ElasticsearchExploration.WebApi.Model;
+using Microsoft.AspNetCore.Mvc;
+using Nest;
+using System;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace ElasticsearchExploration.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ManuscriptsController : ControllerBase
+    {
+        private static readonly IElasticClient elasticClient = BuildElasticClient();
+
+        public ManuscriptsController()
+        {
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddBlogPost()
+        {
+            var manuscript = new Manuscript(Guid.NewGuid(), "Pharrel Williams", 4.20);
+            var indexResponse = await elasticClient.IndexDocumentAsync(manuscript);
+
+            return CreatedAtRoute(nameof(GetBlogPost), new { guid = manuscript.Guid }, manuscript);
+        }
+
+        [HttpGet("{guid}", Name = nameof(GetBlogPost))]
+        public async Task<ActionResult<Manuscript>> GetBlogPost(Guid guid)
+        {
+            var queryResult = await elasticClient.SearchAsync<Manuscript>(s =>
+                s.Query(q => q.Match(m => m.Field(f => f.Guid).Query(guid.ToString())))
+            );
+            return Ok(queryResult.Documents.First());
+        }
+
+        private static IElasticClient BuildElasticClient()
+        {
+            var settings = new ConnectionSettings(new Uri("http://localhost:9200")).DefaultIndex("manuscripts");
+            return new ElasticClient(settings);
+        }
+    }
+}
---
+++ ElasticsearchExploration.WebApi/Model/Manuscript.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ElasticsearchExploration.WebApi.Model
+{
+    public class Manuscript
+    {
+        public Guid Guid { get; }
+        public string AuthorName { get; }
+        public double ImpactFactor { get; }
+
+        public Manuscript(Guid guid, string authorName, double impactfactor)
+        {
+            Guid = guid;
+            AuthorName = authorName;
+            ImpactFactor = impactfactor;
+        }
+    }
+}
